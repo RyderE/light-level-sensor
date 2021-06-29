5f04ec2cbaea813cfc46fcf324c50b0558c0591e
RyderE <86691185+RyderE@users.noreply.github.com>
2021-06-30 10:19:18 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.B, function () {
+    light_level = input.lightLevel()
+    basic.showNumber(light_level)
+})
+let light_level = 0
+basic.showIcon(IconNames.Tortoise)
+basic.forever(function () {
+	
+})
